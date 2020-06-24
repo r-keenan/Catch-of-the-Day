ca402808cc54a6d76668d9d1deff21fb2f74dbe3
Ross Keenan <ross.keenan@protonmail.com>
2020-06-23 19:21:18 -0500

    Initial commit

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,2 +1,7 @@
-// let's go!
-alert('hey');
+import React from "react";
+import { render } from "react-dom";
+import StorePicker from "./components/StorePicker";
+import App from "./components/App";
+import "./css/style.css";
+
+render(<App />, document.querySelector("#main"));
