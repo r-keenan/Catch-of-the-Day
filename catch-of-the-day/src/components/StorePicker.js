648f7328e6f2a7d37c38eb60f3cf9eaf556b9a1c
Ross Keenan <ross.keenan@protonmail.com>
2020-06-23 21:26:01 -0500

    added binding to StorePicker

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -2,15 +2,25 @@ import React from "react";
 import { getFunName } from "../helpers";
 
 class StorePicker extends React.Component {
+  myInput = React.createRef();
+
+  goToStore = (event) => {
+    // 1. Stop the form from submitting
+    event.preventDefault();
+    // 2. Get the text from that input
+    console.log(this);
+    // 3. Change the page to /store/whatever-they-entered
+  }
   render() {
     return (
       <React.Fragment>
         <p>Fish!</p>
-        <form className="store-selector">
+        <form className="store-selector" onSubmit={this.goToStore}>
           {/* This is a comment */}
           <h2>Please Enter A Store</h2>
           <input
             type="text"
+            ref={this.myInput}
             required
             placeholder="Store Name"
             defaultValue={getFunName()}
